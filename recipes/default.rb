73375bac81ace0b48b75b5f6053b2f8a4b593f83
Steve Nolen <technolengy@gmail.com>
2015-12-11 09:53:11 -0800

    add ntp server list and recipe (cookbook was already in dep list)

--- recipes/default.rb
+++ recipes/default.rb
@@ -28,6 +28,15 @@ selinux_state 'SELinux Permissive' do
   action :permissive
 end
 
+node.set['ntp']['servers'] = [
+  'tick.ucla.edu',
+  '0.pool.ntp.org',
+  '1.pool.ntp.org',
+  '2.pool.ntp.org',
+  '3.pool.ntp.org'
+]
+include_recipe 'ntp'
+
 # sudo
 node.set['authorization']['sudo']['sudoers_defaults'] = [
   'env_reset',
